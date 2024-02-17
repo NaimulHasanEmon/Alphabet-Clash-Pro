331ebbd10d9b2f554af48fd6d2aff9ca16fa5a3d
emon12700 <hasan15-5601@diu.edu.bd>
2024-02-18 03:46:32 +0600

    random alphabet generated

--- Script/alpha-clash.js
+++ Script/alpha-clash.js
@@ -1,7 +1,19 @@
+function getARandomAlphabet() {
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = alphabetString.split('');
+    return alphabets[Math.floor(Math.random() * alphabets.length)];
+}
+
+function continueGame() {
+    const alphabet = getARandomAlphabet();
+    return alphabet;
+}
+
 function play() {
     // // Hide the home screen by adding the hidden class to the classList
     hideElementById('home-screen');
-    
+
     // // Show the game screen by removing the hidden class from the classList
     setElementById('play-ground');
+    console.log(continueGame());
 }
